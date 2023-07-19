6cabde4603b1632b918a8d38636ed5febd496f02
rothiotome <rothiotome@gmail.com>
2023-07-19 14:37:17 +0200

    Update CheckIfTextMatches and scene (hope it helps!)

--- Type.2/Assets/Scripts/CheckIfTextMatches.cs
+++ Type.2/Assets/Scripts/CheckIfTextMatches.cs
@@ -1,38 +1,56 @@
+using System;
 using TMPro;
 using UnityEngine;
 
-public class CheckIfTextMatches : MonoBehaviour {
-    [SerializeField]
-    private TMP_InputField PlayInputField;
+public class CheckIfTextMatches : MonoBehaviour
+{
+    [SerializeField] private TMP_InputField PlayInputField;
 
-    [SerializeField]
-    private TMP_InputField SettingsInputField;
+    [SerializeField] private TMP_InputField SettingsInputField;
 
-    [SerializeField]
-    private TMP_InputField AboutInputField;
+    [SerializeField] private TMP_InputField AboutInputField;
 
-    [SerializeField]
-    private TextMeshProUGUI PlayInputFieldPlaceholder;
+    [SerializeField] private TextMeshProUGUI PlayInputFieldPlaceholder;
 
-    [SerializeField]
-    private TextMeshProUGUI SettingsInputFieldPlaceholder;
-    [SerializeField]
-    private PublicAnimations publicAnimations;
-
-    [SerializeField]
-    private TextMeshProUGUI AboutInputFieldPlaceholder;
-     public void checkIfTextMatches(){
+    [SerializeField] private TextMeshProUGUI SettingsInputFieldPlaceholder;
+    
+    [SerializeField] private TextMeshProUGUI AboutInputFieldPlaceholder;
+    
+    [SerializeField] private PublicAnimations publicAnimations;
+    
+    public void checkIfTextMatches()
+    {
         if (PlayInputField.text == PlayInputFieldPlaceholder.text)
-            {
-                Debug.Log("Play");
-            }
-            else if (SettingsInputField.text == SettingsInputFieldPlaceholder.text)
-            {
-                publicAnimations.SettingsAnimation();
-            }
-            else if (AboutInputField.text == AboutInputFieldPlaceholder.text)
-            {
-                Debug.Log("About");
-            }
-     }
+        {
+            Debug.Log("Play");
+        }
+        
+        else if (SettingsInputField.text == SettingsInputFieldPlaceholder.text)
+        {
+            publicAnimations.SettingsAnimation();
+        }
+        
+        else if (AboutInputField.text == AboutInputFieldPlaceholder.text)
+        {
+            Debug.Log("About");
+        }
+        else
+        {
+            //Whatever
+        }
+    }
+
+    public void CheckMatches(string currentValue)
+    {
+        if (currentValue.Equals(PlayInputFieldPlaceholder.text, StringComparison.InvariantCultureIgnoreCase))
+        {
+            Debug.Log("Play");
+        }else if (currentValue.Equals(SettingsInputFieldPlaceholder.text, StringComparison.InvariantCultureIgnoreCase))
+        {
+            publicAnimations.SettingsAnimation();
+        }else if (currentValue.Equals(AboutInputFieldPlaceholder.text, StringComparison.InvariantCultureIgnoreCase))
+        {
+            Debug.Log("About");
+        }
+    }
 }
